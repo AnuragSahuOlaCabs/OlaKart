68ec8e8452d897c5eb80c7929fc9aecef2e4060d
Akash Mishra1 <akash.mishra1@olacabs.com>
2022-07-06 12:47:27 +0530

    implemented seller registration

--- src/main/java/com/example/controller/SellerController.java
+++ src/main/java/com/example/controller/SellerController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entities.LoginCredentials;
 import com.example.entities.Seller;
+import com.example.entities.User;
 import com.example.service.SellerService;
 
 @RestController
@@ -17,14 +19,26 @@ public class SellerController {
 
 	@Autowired
 	private SellerService sellerService;
+	
 	@GetMapping("/sellers")
 	public List<Seller> getAllSellers(){
 		return sellerService.getAllSellers();
 	}
 	
+	@GetMapping("/sellers/{id}")
+	public Seller getSellerbyId(@PathVariable("id") int id) throws Exception {
+		return sellerService.getSellerById(id);
+	}
+	
 	@PostMapping("/login_seller")
 	public List<Seller> chkSeller(@RequestBody LoginCredentials loginCre) throws Exception {
 		return this.sellerService.chkSeller(loginCre);	
+	}
+	
+	@PostMapping("/register_seller")
+	public Seller addSeller(@RequestBody Seller seller) throws Exception {
+		Seller s = this.sellerService.saveOrUpdate(seller);
+		return s;
 	}
 
 }

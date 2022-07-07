696b6f6312ec4925b8326267282efc86bff7ab62
Rakshita <rakshitasharma47@gmail.com>
2022-07-07 17:11:37 +0530

    Correct pojos

--- src/main/java/com/example/entities/Seller.java
+++ src/main/java/com/example/entities/Seller.java
@@ -13,14 +13,20 @@ public class Seller {
 	private String password;
 	private String phone_number;
 	private String store_address;
+	private String salt;
+	private String photo;
+	
 	
 	
 	public Seller() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
+
+
 	public Seller(int store_id, String email_id, String store_name, String seller_name, String password,
-			String phone_number, String store_address) {
+			String phone_number, String store_address, String salt, String photo) {
 		super();
 		this.store_id = store_id;
 		this.email_id = email_id;
@@ -29,56 +35,130 @@ public class Seller {
 		this.password = password;
 		this.phone_number = phone_number;
 		this.store_address = store_address;
+		this.salt = salt;
+		this.photo = photo;
 	}
+
+
+
 	public int getStore_id() {
 		return store_id;
 	}
+
+
+
 	public void setStore_id(int store_id) {
 		this.store_id = store_id;
 	}
+
+
+
 	public String getEmail_id() {
 		return email_id;
 	}
+
+
+
 	public void setEmail_id(String email_id) {
 		this.email_id = email_id;
 	}
+
+
+
 	public String getStore_name() {
 		return store_name;
 	}
+
+
+
 	public void setStore_name(String store_name) {
 		this.store_name = store_name;
 	}
+
+
+
 	public String getSeller_name() {
 		return seller_name;
 	}
+
+
+
 	public void setSeller_name(String seller_name) {
 		this.seller_name = seller_name;
 	}
+
+
+
 	public String getPassword() {
 		return password;
 	}
+
+
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+
+
 	public String getPhone_number() {
 		return phone_number;
 	}
+
+
+
 	public void setPhone_number(String phone_number) {
 		this.phone_number = phone_number;
 	}
+
+
+
 	public String getStore_address() {
 		return store_address;
 	}
+
+
+
 	public void setStore_address(String store_address) {
 		this.store_address = store_address;
 	}
+
+
+
+	public String getSalt() {
+		return salt;
+	}
+
+
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+
+
+	public String getPhoto() {
+		return photo;
+	}
+
+
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+
+
 	@Override
 	public String toString() {
 		return "Seller [store_id=" + store_id + ", email_id=" + email_id + ", store_name=" + store_name
 				+ ", seller_name=" + seller_name + ", password=" + password + ", phone_number=" + phone_number
-				+ ", store_address=" + store_address + "]";
+				+ ", store_address=" + store_address + ", salt=" + salt + ", photo=" + photo + ", getStore_id()="
+				+ getStore_id() + ", getEmail_id()=" + getEmail_id() + ", getStore_name()=" + getStore_name()
+				+ ", getSeller_name()=" + getSeller_name() + ", getPassword()=" + getPassword() + ", getPhone_number()="
+				+ getPhone_number() + ", getStore_address()=" + getStore_address() + ", getSalt()=" + getSalt()
+				+ ", getPhoto()=" + getPhoto() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
 	}
 	
-	
-
 }
--- src/main/java/com/example/entities/User.java
+++ src/main/java/com/example/entities/User.java
@@ -7,12 +7,14 @@ import javax.persistence.Id;
 public class User {
 	@Id
 	private int user_id;
-	private String FirstName;
+	private String first_name;
 	private String email_id;
 	private String password_en;
 	private String phone_number;
 	private String gender;
+	private String photo;
 	private String address;
+	private int salt;
 	private int wallet;
 	/**
 	 * @param user_id
@@ -22,17 +24,35 @@ public class User {
 	 * @param gender
 	 * @param address
 	 */
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(int user_id, String first_name, String email_id, String password_en, String phone_number, String gender,
+			String photo, String address, int salt, int wallet) {
+		super();
+		this.user_id = user_id;
+		this.first_name = first_name;
+		this.email_id = email_id;
+		this.password_en = password_en;
+		this.phone_number = phone_number;
+		this.gender = gender;
+		this.photo = photo;
+		this.address = address;
+		this.salt = salt;
+		this.wallet = wallet;
+	}
 	public int getUser_id() {
 		return user_id;
 	}
 	public void setUser_id(int user_id) {
 		this.user_id = user_id;
 	}
-	public String getFirstName() {
-		return FirstName;
+	public String getFirst_name() {
+		return first_name;
 	}
-	public void setFirstName(String firstName) {
-		FirstName = firstName;
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
 	}
 	public String getEmail_id() {
 		return email_id;
@@ -58,11 +78,23 @@ public class User {
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
 	public String getAddress() {
 		return address;
 	}
 	public void setAddress(String address) {
 		this.address = address;
+	}
+	public int getSalt() {
+		return salt;
+	}
+	public void setSalt(int salt) {
+		this.salt = salt;
 	}
 	public int getWallet() {
 		return wallet;
@@ -72,30 +104,14 @@ public class User {
 	}
 	@Override
 	public String toString() {
-		return "User [user_id=" + user_id + ", FirstName=" + FirstName + ", email_id=" + email_id + ", password_en="
-				+ password_en + ", phone_number=" + phone_number + ", gender=" + gender + ", address=" + address
-				+ ", wallet=" + wallet + ", getUser_id()=" + getUser_id() + ", getFirstName()=" + getFirstName()
-				+ ", getEmail_id()=" + getEmail_id() + ", getPassword_en()=" + getPassword_en() + ", getPhone_number()="
-				+ getPhone_number() + ", getGender()=" + getGender() + ", getAddress()=" + getAddress()
+		return "User [user_id=" + user_id + ", first_name=" + first_name + ", email_id=" + email_id + ", password_en="
+				+ password_en + ", phone_number=" + phone_number + ", gender=" + gender + ", photo=" + photo
+				+ ", address=" + address + ", salt=" + salt + ", wallet=" + wallet + ", getUser_id()=" + getUser_id()
+				+ ", getFirst_name()=" + getFirst_name() + ", getEmail_id()=" + getEmail_id() + ", getPassword_en()="
+				+ getPassword_en() + ", getPhone_number()=" + getPhone_number() + ", getGender()=" + getGender()
+				+ ", getPhoto()=" + getPhoto() + ", getAddress()=" + getAddress() + ", getSalt()=" + getSalt()
 				+ ", getWallet()=" + getWallet() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
 				+ ", toString()=" + super.toString() + "]";
 	}
-	public User(int user_id, String firstName, String email_id, String password_en, String phone_number, String gender,
-			String address, int wallet) {
-		super();
-		this.user_id = user_id;
-		FirstName = firstName;
-		this.email_id = email_id;
-		this.password_en = password_en;
-		this.phone_number = phone_number;
-		this.gender = gender;
-		this.address = address;
-		this.wallet = wallet;
-	}
-	public User() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	
+		
 }

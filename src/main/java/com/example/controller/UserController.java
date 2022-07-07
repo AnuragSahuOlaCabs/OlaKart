14a6b38c390962a6b06634d91bfd088c96f2ee41
Anurag Sahu <anurag.sahu@olacabs.com>
2022-07-07 17:35:39 +0530

    Adding @CrossOrigin to make it work with Frontend

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.entities.*;
 import com.example.service.ECommService;
 
+@CrossOrigin
 @RestController
 public class UserController {
 	@Autowired
